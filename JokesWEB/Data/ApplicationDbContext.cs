6e775ceabe0562817b81669389b1eacffdae5354
medeirosdev <guilherme.ggcm20@gmail.com>
2023-03-02 09:10:50 -0300

    Adicionar arquivos de projeto.

---
+++ JokesWEB/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using JokesWEB.Models;
+
+namespace JokesWEB.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<JokesWEB.Models.Joke> Joke { get; set; }
+    }
+}
---
+++ JokesWEB/Models/Joke.cs
@@ -0,0 +1,18 @@
+﻿namespace JokesWEB.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+
+        public string JokeQuestion { get; set; }
+
+        public string JokeAnswer { get; set; }
+
+
+        public Joke()
+        {
+
+        }
+
+    }
+}
